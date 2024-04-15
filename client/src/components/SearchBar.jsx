d4681aa493c2df4fde3dc3741b1a2f43f8637059
AXEL JANIAK <axeljpro@gmail.com>
2024-04-15 17:03:46 +0200

    fix request from PR add label and figcaption

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,45 +1,58 @@
 import { useState } from "react";
 
 function SearchBar() {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
   const apiToken = import.meta.env.VITE_TOKEN_API;
 
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${apiToken}`
-    }
+      accept: "application/json",
+      Authorization: `Bearer ${apiToken}`,
+    },
   };
 
   const getMovie = (url) => {
     fetch(url, options)
-      .then(response => response.json())
-      .then(data => setMovies(data.results)
-      )
-      .catch(err => console.error(err));
-  }
+      .then((response) => response.json())
+      .then((data) => setMovies(data.results))
+      .catch((err) => console.error(err));
+  };
 
   const handleChange = (e) => {
     const searchValue = e.target.value;
-      getMovie(`https://api.themoviedb.org/3/search/multi?language=fr-FR&query=${searchValue}`)
-  }
+    getMovie(
+      `https://api.themoviedb.org/3/search/multi?language=fr-FR&query=${searchValue}`
+    );
+  };
 
   return (
     <>
-      <input type="search" placeholder="Rechercher" onChange={handleChange} />
+      <label htmlFor="search">Rechercher</label>
+      <input
+        name="search"
+        id="search"
+        type="search"
+        placeholder="Rechercher"
+        onChange={handleChange}
+      />
       <section>
-      {movies.map(movie => (
-        <article key={movie.id}>
-          <figure>
-            <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title}/>
-          </figure>
-        </article>
-        
-      ))}
+        {movies.map((movie) => (
+          <article key={movie.id}>
+            <figure>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <figcaption>
+                {movie.title} {movie.name}
+              </figcaption>
+            </figure>
+          </article>
+        ))}
       </section>
     </>
-  )
+  );
 }
 
 export default SearchBar;
