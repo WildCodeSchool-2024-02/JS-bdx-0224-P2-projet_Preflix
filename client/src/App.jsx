681fd519ccefc486405a45cda11541406c9b1155
Delgorithm <delgorithm@gmail.com>
2024-04-17 11:20:44 +0200

    Feature : Added the components needed for the display : Btn + Searchbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,49 @@
 import { useState } from "react";
 import "./App.css";
-import ButtonMovies from "./components/ButtonMovies";
+import Btn from "./components/Btn";
+import SearchBar from "./components/Searchbar";
 
 function App() {
-  const [movies, setMovies] = useState([]);
+  const apiToken = import.meta.env.VITE_API_TOKEN;
+  const urlDiscoverSeries =
+    "https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc";
+  const urlDiscoverMovies =
+    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&page=1&sort_by=popularity.desc";
 
-  return <ButtonMovies labelMovies="Movies" movies={movies} setMovies={setMovies} />
-    
-  
+  const urlSearch =
+    "https://api.themoviedb.org/3/search/multi?language=fr-FR&query";
+
+  const [series, setSeries] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [currentSearch, setCurrentSearch] = useState([]);
+  const [isSeriesVisible, setIsSeriesVisible] = useState(false);
+  const [isMoviesVisible, setIsMoviesVisible] = useState(false);
+
+  return (
+    <section>
+      <SearchBar
+        fetchData={{ url: urlSearch, data: currentSearch }}
+        setData={setCurrentSearch}
+        apiToken={apiToken}
+      />
+      <Btn
+        label="Séries"
+        fetchData={{ url: urlDiscoverSeries, data: series }}
+        setData={setSeries}
+        apiToken={apiToken}
+        setIsVisible={setIsSeriesVisible}
+        isVisible={isSeriesVisible}
+      />
+      <Btn
+        label="Films"
+        fetchData={{ url: urlDiscoverMovies, data: movies }}
+        setData={setMovies}
+        apiToken={apiToken}
+        setIsVisible={setIsMoviesVisible}
+        isVisible={isMoviesVisible}
+      />
+    </section>
+  );
 }
 
 export default App;
