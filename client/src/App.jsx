d859afcf85834caf7eb9f773993f13819e97fbac
AXEL JANIAK <axeljpro@gmail.com>
2024-04-17 13:05:08 +0200

    css searchbar mobile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
-import SearchBar from "./components/SearchBar";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <SearchBar />;
+  return (
+      <NavBar />
+  );
 }
 
 export default App;
