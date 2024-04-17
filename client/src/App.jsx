6ff55b91a7231fff6c752d6b65d5006a846507d3
AXEL JANIAK <axeljpro@gmail.com>
2024-04-17 19:11:19 +0200

    add feature icon change colour

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
+    <>
+    <NavBar/>
     <main>
       <Outlet />
     </main>
+    </>
   );
 }
 
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,41 @@
+import { Link, useLocation } from "react-router-dom";
+
 import SearchBar from "./SearchBar";
 
+
 function NavBar() {
+
+  const location = useLocation();
+
+  const getIconHome = () => {
+    switch (location.pathname) {
+      case '/':
+        return 'iconsaccueilyellow.svg';
+        default :
+        return 'icons-accueil.svg';
+      }
+    };
+
+    const getIconSearch = () => {
+      switch (location.pathname) {
+        case '/search':
+          return 'icons-search-yellow.svg';
+          default :
+          return 'icons-chercher.svg';
+        }
+      };
+
+    const getIconCredits = () => {
+      switch (location.pathname) {
+        case '/credits':
+          return 'heart-hand-shake-yellow.svg';
+          default :
+          return 'heart-hand-shake.svg';
+        }
+      };
+    
   return (
-    <section className="nav">
+    <nav className="nav">
       <img
         className="logo"
         src="../src/assets/images/logo.svg"
@@ -10,27 +43,33 @@ function NavBar() {
       />
       <ul className="navList">
         <li>
-          <img
-            className="navIcon"
-            src="../src/assets/images/icons-accueil.svg"
-            alt="Accueil"
-          />
-          <h2 className="titleNav">Accueil</h2>
+          <Link className="linkName" to="/">
+            <img
+              className="navIcon"
+              src={`../src/assets/images/${getIconHome()}`}
+              alt="Accueil"
+            />
+            <h2 className="titleNav">Accueil</h2>
+          </Link>
         </li>
         <li>
-          <img
-            className="navIcon"
-            src="../src/assets/images/icons-chercher.svg"
-            alt="Rechercher"
-          />
+          <Link className="linkName linkSearch" to="/search">
+            <img
+              className="navIcon"
+              src={`../src/assets/images/${getIconSearch()}`}
+              alt="Rechercher"
+            />
+          </Link>
         </li>
         <li>
-          <img
-            className="navIcon"
-            src="../src/assets/images/heart-hand-shake.svg"
-            alt="Crédits"
-          />
-          <h2 className="titleNav">Crédits</h2>
+          <Link className="linkName" to="/credits">
+            <img
+              className="navIcon"
+              src={`../src/assets/images/${getIconCredits()}`}
+              alt="Crédits"
+            />
+            <h2 className="titleNav">Crédits</h2>
+          </Link>
         </li>
         <li className="navDesktop titleNav">
           <h2>Séries</h2>
@@ -39,13 +78,15 @@ function NavBar() {
           <h2>Films</h2>
         </li>
         <li className="navDesktop titleNav">
-          <h2>Catégories</h2>
+          <Link className="linkName" to="category">
+            <h2>Catégories</h2>
+          </Link>
         </li>
       </ul>
       <form className="searchNavBar">
         <SearchBar />
       </form>
-    </section>
+    </nav>
   );
 }
 
