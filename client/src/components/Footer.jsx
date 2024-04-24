87e90052eb30cceec729fc82911795ae39fedbcf
AXEL JANIAK <axeljpro@gmail.com>
2024-04-24 09:58:20 +0200

    fix img

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "../Styles/Footer.css";
 function Footer() {
   return (
     <footer>
-      <img src="../src/assets/images/logo-tmdb.svg" alt="TMDB logo" />
+      <img className="logoTmdb" src="../src/assets/images/logo-tmdb.svg" alt="TMDB logo" />
       <p>PREFLIX © 2024</p>
       <p>Conditions d'utilisation - Politique de confidentialité</p>
     </footer>
