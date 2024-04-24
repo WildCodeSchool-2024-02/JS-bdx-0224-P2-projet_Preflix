98ae001bca1b48f845750aa3ec72bcb73c3156a8
ALICE BERTHELOT <aliceberthelot.pro@gmail.com>
2024-04-24 10:25:17 +0200

    fix Movies.jsx and Series.jsx

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ArrowBack from "../components/ArrowBack";
-import "../Styles/Btn.css"
+import "../Styles/Btn.css";
 
 function Movies() {
-
   const apiToken = import.meta.env.VITE_API_TOKEN;
-  
+
   const [movies, setMovies] = useState([]);
 
+  useEffect(() => {
     const options = {
       method: "GET",
       headers: {
@@ -15,33 +15,35 @@ function Movies() {
         Authorization: `Bearer ${apiToken}`,
       },
     };
-    
-    fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&page=1&sort_by=popularity.desc', options)
+
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&page=1&sort_by=popularity.desc",
+      options
+    )
       .then((response) => response.json())
       .then((data) => setMovies(data.results))
       .catch((err) => console.error(err));
-    
+  }, [apiToken]);
 
   return (
-      <>
+    <>
       <ArrowBack />
       <h2 className="title-movies">FILMS</h2>
-        <section className="section-movies">
-          {movies.map((item) => (
-            <article key={item.id}>
-              <figure>
-                <img
-                  src={`https://image.tmdb.org/t/p/original${item.poster_path}`}
-                  alt={item.title}
-                  className="posterMovie"
-                />
-              </figure>
-            </article>
-          ))}
-        </section>
-  </>
+      <section className="section-movies">
+        {movies.map((item) => (
+          <article key={item.id}>
+            <figure>
+              <img
+                src={`https://image.tmdb.org/t/p/original${item.poster_path}`}
+                alt={item.title}
+                className="posterMovie"
+              />
+            </figure>
+          </article>
+        ))}
+      </section>
+    </>
   );
-
 }
 
 export default Movies;
--- client/src/pages/Series.jsx
+++ client/src/pages/Series.jsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ArrowBack from "../components/ArrowBack";
-import "../Styles/Btn.css"
+import "../Styles/Btn.css";
 
 function Series() {
-
   const apiToken = import.meta.env.VITE_API_TOKEN;
-  
+
   const [series, setSeries] = useState([]);
 
+  useEffect(() => {
     const options = {
       method: "GET",
       headers: {
@@ -15,33 +15,35 @@ function Series() {
         Authorization: `Bearer ${apiToken}`,
       },
     };
-    
-    fetch('https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc', options)
+
+    fetch(
+      "https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc",
+      options
+    )
       .then((response) => response.json())
       .then((data) => setSeries(data.results))
       .catch((err) => console.error(err));
-    
+  }, [apiToken]);
 
   return (
-      <>
+    <>
       <ArrowBack />
       <h2 className="title-movies">SERIES</h2>
-        <section className="section-movies">
-          {series.map((item) => (
-            <article key={item.id}>
-              <figure>
-                <img
-                  src={`https://image.tmdb.org/t/p/original${item.poster_path}`}
-                  alt={item.title}
-                  className="posterMovie"
-                />
-              </figure>
-            </article>
-          ))}
-        </section>
-  </>
+      <section className="section-movies">
+        {series.map((item) => (
+          <article key={item.id}>
+            <figure>
+              <img
+                src={`https://image.tmdb.org/t/p/original${item.poster_path}`}
+                alt={item.title}
+                className="posterMovie"
+              />
+            </figure>
+          </article>
+        ))}
+      </section>
+    </>
   );
-
 }
 
 export default Series;
