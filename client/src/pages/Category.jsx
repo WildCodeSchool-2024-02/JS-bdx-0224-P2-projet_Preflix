2ca64c919b2613eaa1c51229749489ff2d0c5f09
Delgorithm <delgorithm@gmail.com>
2024-04-26 02:15:48 +0200

    fix some path for the description

--- client/src/pages/Category.jsx
+++ client/src/pages/Category.jsx
@@ -1,5 +1,5 @@
 import { useState, useContext, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import ArrowBack from "../components/ArrowBack";
 import { CategoryContext } from "../contexts/CategoryContext";
 import "../Styles/Category.css";
@@ -9,7 +9,7 @@ function Category() {
 
   const { categoryList } = useParams();
   const categoryName = categoryList;
-  
+
   const selectedCategoryId = types
     .filter((type) => type.name === categoryName)
     .map((selection) => selection.id);
@@ -20,29 +20,29 @@ function Category() {
   const apiToken = import.meta.env.VITE_API_TOKEN;
 
   useEffect(() => {
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${apiToken}`,
-    },
-  };
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${apiToken}`,
+      },
+    };
 
-  fetch(
-    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&page=1&sort_by=popularity.desc",
-    options
-  )
-    .then((response) => response.json())
-    .then((data) => setMovieInfos(data.results))
-    .catch((err) => console.error(err));
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&page=1&sort_by=popularity.desc",
+      options
+    )
+      .then((response) => response.json())
+      .then((data) => setMovieInfos(data.results))
+      .catch((err) => console.error(err));
 
-  fetch(
-    "https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=fr-FR&page=1&sort_by=popularity.desc",
-    options
-  )
-    .then((response) => response.json())
-    .then((data) => setSerieInfos(data.results))
-    .catch((err) => console.error(err));
+    fetch(
+      "https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=fr-FR&page=1&sort_by=popularity.desc",
+      options
+    )
+      .then((response) => response.json())
+      .then((data) => setSerieInfos(data.results))
+      .catch((err) => console.error(err));
   }, [apiToken]);
 
   return (
@@ -56,15 +56,17 @@ function Category() {
               selectedCategoryId.includes(genreId)
             ) && (
               <article key={movie.id}>
-                <figure>
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                    alt={movie.title}
-                  />
-                  <figcaption>
-                    {movie.title} {movie.name}
-                  </figcaption>
-                </figure>
+                <Link to={`/media/movie/${movie.id}`}>
+                  <figure>
+                    <img
+                      src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                      alt={movie.title}
+                    />
+                    <figcaption>
+                      {movie.title} {movie.name}
+                    </figcaption>
+                  </figure>
+                </Link>
               </article>
             )
         )}
@@ -74,15 +76,17 @@ function Category() {
               selectedCategoryId.includes(genreId)
             ) && (
               <article key={serie.id}>
-                <figure>
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500/${serie.poster_path}`}
-                    alt={serie.title}
-                  />
-                  <figcaption>
-                    {serie.title} {serie.name}
-                  </figcaption>
-                </figure>
+                <Link to={`/media/tv/${serie.id}`}>
+                  <figure>
+                    <img
+                      src={`https://image.tmdb.org/t/p/w500/${serie.poster_path}`}
+                      alt={serie.title}
+                    />
+                    <figcaption>
+                      {serie.title} {serie.name}
+                    </figcaption>
+                  </figure>
+                </Link>
               </article>
             )
         )}
